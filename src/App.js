da262bd0a0b035ee1940f19dfcdeb6958f4c9deb
VenkatSaaii <saiboda520@gmail.com>
2024-03-31 20:13:13 +0530

    react1project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
+import UserForm from './components/User/Userform'
+import UserList from './components/User/UserList';
+
 
 function App() {
+  const [userslist,setUsersList]=useState([]);
+
+  const submitHandler=(uName ,uAge)=>{
+    setUsersList((prevUsersList) =>{
+      return[...prevUsersList , {name:uName ,age:uAge , id:Math.random().toString()}]
+      
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div>
+      <UserForm onAddUser={submitHandler}/>
+      <UserList users={userslist}/>
     </div>
+    
   );
 }
 
